573effe996bfb247a91c205e6bfd6c4d48d832d5
focusdroid <840254112@qq.com>
2023-02-28 23:02:55 +0800

    featrue(add):30230225限制权限表类型

--- models/auth.go
+++ models/auth.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type Auth struct {
 	gorm.Model
-	User          string
-	Email         string
-	Phone         int
-	AuthType      string
-	AuthNumber    int
-	AuthInterface string
+	User          string `gorm:"type:varchar(255)" json:"user"`
+	Email         string `gorm:"type:varchar(255)" json:"email"`
+	Phone         int    `gorm:"type:varchar(11)" json:"phone"`
+	AuthType      string `gorm:"type:varchar(255)" json:"auth_type"`
+	AuthNumber    int    `gorm:"type:varchar(255)" json:"auth_number"`
+	AuthInterface string `gorm:"type:varchar(255)" json:"auth_interface"`
 }
 
 func (auth Auth) TableName() string {
