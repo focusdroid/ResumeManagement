74655824ebda350bc518ccc6b07eb2aa182a6ec1
focusdroid <840254112@qq.com>
2023-03-01 14:06:35 +0800

    featrue(modify):20230225修改数据库类型标注

--- models/auth.go
+++ models/auth.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type Auth struct {
 	gorm.Model
-	User          string `gorm:"type:varchar(255)" json:"user"`
-	Email         string `gorm:"type:varchar(255)" json:"email"`
-	Phone         int    `gorm:"type:varchar(11)" json:"phone"`
-	AuthType      string `gorm:"type:varchar(255)" json:"auth_type"`
-	AuthNumber    int    `gorm:"type:varchar(255)" json:"auth_number"`
-	AuthInterface string `gorm:"type:varchar(255)" json:"auth_interface"`
+	User          string `gorm:"type:varchar(255)" from:"user" db:"column:user"  json:"user" comment:"用户名称"`
+	Email         string `gorm:"type:varchar(255)" from:"email" db:"column:email"  json:"email" comment:"邮箱"`
+	Phone         int    `gorm:"type:varchar(11)"  from:"phone" db:"column:phone"  json:"phone" comment:"手机号"`
+	AuthType      string `gorm:"type:varchar(255)" from:"auth_type" db:"column:auth_type"  json:"auth_type" comment:"权限类型"`
+	AuthNumber    int    `gorm:"type:varchar(255)" from:"auth_number" db:"column:auth_number"  json:"auth_number" comment:"权限等级"`
+	AuthInterface string `gorm:"type:varchar(255)" from:"auth_interface" db:"column:auth_interface"  json:"auth_interface" comment:"该用户对应的权限"`
 }
 
 func (auth Auth) TableName() string {
